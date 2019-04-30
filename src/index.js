542917fab6ae87dd77e9a672ddf97e0e1045a2df
zhangjingshi <83528694@qq.com>
2019-04-30 21:49:00 +0800

    jquery引入

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
  import _ from 'lodash';
-
+ import j from 'jquery'
   function component() {
-    var element = document.createElement('div');
+    var element = j('<div></div>');
+   /* lodash is required for the next line to work */
+   element.html(_.join(['Hello','webpack'], ' '))
+     return element.get(0);
 
-   
-   // Lodash, now imported by this script
-    element.innerHTML = _.join(['Hello', 'webpack'], ' ');
 
-    return element;
   }
 
   document.body.appendChild(component());
